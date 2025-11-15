aa65238eedd31f0d23cfd46d82446b4f2ab8d594
IS250 Student <changeme@example.com>
2025-11-15 12:25:01 -0600

    Add pseudocode for the program

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 """
-Full Name:
-Class-Section:
-Assignment Title:
-Submission Date:
+Kou Yang
+IS-250
+Calculate Average of Three Scores
+11/15/2025
 """
 """
-Write your pseudocode here.
-Do not write any Python code in this section.
-Your pseudocode should describe your overall approach in your own words.
+    
+Ask user to enter first number as num1:
+Ask user to enter second number as num2:
+Ask user to enter third number as num3:
+
+function average to calculate average of num1 and num2 and num3:
+    Display first number:
+    Display second number:
+    Display third number:
+    Calculate the sum of num1 and num2 and num3 and then divide by 3
+    
+Call the function average to display the result.
+
 """
 
 # Your Python code begins below this line.
